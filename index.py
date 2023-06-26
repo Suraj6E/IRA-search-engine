3a2628fd602697d2b9864f758a2b1cbb1ab1baaf
suraj.neupane <suraj.neupane151@gmail.com>
2023-06-26 19:28:40 +0545

    search bar with search results

--- index.py
+++ index.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Making changes here</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    # Perform search operation with the query
+    # You can add your search logic here
+    return f"Search results for: {query}"
+
+if __name__ == '__main__':
+    app.run()

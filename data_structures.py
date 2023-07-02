eb1c37339ca8e05cf24a1c92d8b270f4687e68ea
suraj.neupane <suraj.neupane151@gmail.com>
2023-07-02 20:41:48 +0545

    indexing of search words implemented

--- data_structures.py
+++ data_structures.py
@@ -1,12 +1,20 @@
 #create indexes 
 import pandas as pandas;
-
-import nltk
-nltk.download('wordnet')
 import re
+import nltk
+import json
 
+nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+
+
+from collections import defaultdict
+
+index_filename = 'search_indexs.json';
 
 
 #should be called after new data is arrived
@@ -18,7 +26,7 @@ def get_data_size():
     return df.size;
 
 
-def create_index(df):
+def add_index_col(df):
     if 'id' not in df.columns:
         df.insert(0, 'id', df.reset_index().index)
         df.to_csv('data.csv', index=False);
@@ -29,21 +37,60 @@ def process_word(df):
 
     # Remove non-alphanumeric characters and make words lowercase for each words
     def lemmatize_word(word):
-        word = re.sub(r'\W+', ' ', word)
+
+        #collection of stopwords
+        stop_words = set(stopwords.words('english'))
+        
         word = word.lower();
+
+        #filter stop words
+        if(word in stop_words): return '';
+
+        word = re.sub(r'\W+', ' ', word)
         return lemmatizer.lemmatize(word)
-    
+
 
     df['title'] = df['title'].apply(lambda x: ' '.join([lemmatize_word(word) for word in x.split()]));
     df['all_authors'] = df['all_authors'].apply(lambda x: ' '.join([lemmatize_word(word) for word in x.split()]));
 
     return df;
 
+def create_indexes(df):
+    word_dict = defaultdict(list);
+    for index, row in df.iterrows():
+        title = row['title'];
+        authors = row['all_authors'];
+        words = title.split() + authors.split();
+        for word in words:
+            word_dict[word].append(index)
+    return word_dict;
 
 
-df = create_index(get_df());
-print(df.head());
+#utilized everything on this file
+def create_save_indexs():
+    try: 
+        df = add_index_col(get_df());
+        df = process_word(df);
+        indexes = create_indexes(df);
 
-df = process_word(df);
-# df.to_csv('data.csv', index=False)
-print(df.head());
+        print(indexes);
+        #save indexes
+        with open(index_filename, 'w') as new_f:
+            json.dump(indexes, new_f, sort_keys=True, indent=4)
+        
+        return True;
+    except Exception as e:
+        print(f"An error occurred on indexing: {str(e)}")
+        return False;
+
+def read_indexs():
+    date = []
+    with open(index_filename, 'r') as file:
+        data = json.load(file)
+    
+    return data;
+
+
+create_save_indexs();
+
+#single_value = df.loc[0:1].copy();
